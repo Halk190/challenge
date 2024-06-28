56c0cba6db7ae9a8f6813dea6f603e2d9e9e514c
Angel Rojas <halkdavid190@gmail.com>
2024-06-28 17:50:58 -0500

    finish

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
-function encriptar(){
-    let text = document.getElementById("textConvert");
+function encriptar() {
+    let textElement = document.getElementById("textConvert");
+    let text = textElement.value;
+    
+    // Reemplazos para encriptar
+    let encriptado = text.replace(/e/g, "enter")
+                        .replace(/i/g, "imes")
+                        .replace(/a/g, "ai")
+                        .replace(/o/g, "ober")
+                        .replace(/u/g, "ufat");
+
+    // Actualiza el texto en el div de salida
+    document.querySelector('.output_result_text').innerText = encriptado;
+
+    // Muestra el div de resultado y oculta el div de mensaje inicial
+    document.querySelector('.output_clear').classList.add('hidden');
+    document.querySelector('.output_result').classList.remove('hidden');
+    document.querySelector('.output_result').classList.add('show');
 }
+
+function desencriptar() {
+    let textElement = document.getElementById("textConvert");
+    let text = textElement.value;
+
+    // Reemplazos para desencriptar
+    let desencriptado = text.replace(/enter/g, "e")
+                            .replace(/imes/g, "i")
+                            .replace(/ai/g, "a")
+                            .replace(/ober/g, "o")
+                            .replace(/ufat/g, "u");
+
+    // Actualiza el texto en el div de salida
+    document.querySelector('.output_result_text').innerText = desencriptado;
+
+    // Muestra el div de resultado y oculta el div de mensaje inicial
+    document.querySelector('.output_clear').classList.add('hidden');
+    document.querySelector('.output_result').classList.remove('hidden');
+    document.querySelector('.output_result').classList.add('show');
+}
+
+function copiarTexto() {
+    const texto = document.querySelector('.output_result_text').innerText;
+    navigator.clipboard.writeText(texto);
+    alert('Texto copiado al portapapeles');
+}
+
+// Ajustar el tamaño del textarea automáticamente
+function autoResize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+}
+
+// Agregar el evento input para ajustar el tamaño del textarea automáticamente
+document.getElementById('textConvert').addEventListener('input', function() {
+    autoResize(this);
+});
